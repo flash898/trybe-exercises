a7a940873073e2c6ea34896de581861441f2ae2c
Thyago Pessoa <flash_thyago@hotmail.com>
2021-03-24 13:27:43 -0300

    exercicio 2

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
@@ -1,28 +1,42 @@
 /* 1 - Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. Faça programas para: adição, subtração, multiplicação, divisão e módulo.*/
 
-let a = 2;
-let b = 3;
-let op = "divisão";
+// let a = 2;
+// let b = 3;
+// let op = "divisão";
 
-switch(op){
-  case "adição":
-    console.log(a+b);
-    break;
+// switch(op){
+//   case "adição":
+//     console.log(a+b);
+//     break;
 
-  case "subtração":
-    console.log(a-b);
-    break;
+//   case "subtração":
+//     console.log(a-b);
+//     break;
   
-  case "multiplicação":
-    console.log(a*b);
-    break;
+//   case "multiplicação":
+//     console.log(a*b);
+//     break;
 
-  case "divisão":
-    console.log(a/b);
-    break;
+//   case "divisão":
+//     console.log(a/b);
+//     break;
 
-  case "módulo":
-    console.log(a%b);
-    break;
-}
+//   case "módulo":
+//     console.log(a%b);
+//     break;
+// }
+
+// 2- Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados. // 
+
+let num1 = 10;
+let num2 = 10;
+
+if (num2 > num1) {
+  console.log(num2 + " esse é o maior número!");
+} else if (num1 > num2) {
+  console.log(num1 + " esse é o maior número!");
+} else if (num1 = num2) {
+  console.log("Os números possuem o mesmo valor!");
+} 
+
 
