74d4c10bd58312bcba587f9f9399973caa5e1747
Thyago Pessoa <flash_thyago@hotmail.com>
2021-03-24 18:38:18 -0300

    exercicio 4

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
@@ -42,17 +42,26 @@
 
 // 3- Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.//
 
-let num1 = 10;
-let num2 = 1;
-let num3 = 15; 
+// let num1 = 10;
+// let num2 = 1;
+// let num3 = 15; 
 
-if (num1 > num2 && num1 > num3){
-  console.log(num1 + " esse é o maior!");
-} else if (num2 > num1 && num2 > num3){
-  console.log(num2 + " esse é o maior!");
-} else if (num3 > num1 && num3 > num2){
-  console.log(num3 + " esse é o maior!");
-} else if (num1 = num2 = num3){
-  console.log("Números iguais");
+// if (num1 > num2 && num1 > num3){
+//   console.log(num1 + " esse é o maior!");
+// } else if (num2 > num1 && num2 > num3){
+//   console.log(num2 + " esse é o maior!");
+// } else if (num3 > num1 && num3 > num2){
+//   console.log(num3 + " esse é o maior!");
+// } else if (num1 = num2 = num3){
+//   console.log("Números iguais");
+// }
+
+// 4- Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.//
+
+let num1 = -1;
+
+if (num1 >= 0){
+  console.log("Positive");
+} else {
+  console.log("Negative");
 }
-
