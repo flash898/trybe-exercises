c9ce17f820fcaf03064286f0afe682605cd81082
Thyago Pessoa <flash_thyago@hotmail.com>
2021-03-25 16:07:16 -0300

    exercicio 2

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/script.js
@@ -2,11 +2,18 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // 1- Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
 
-for (let number of numbers){
-  console.log(number);
-}
+// for (let number of numbers){
+//   console.log(number);
+// }
 
 // 2- Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;
+
+let resultado = 0;
+for (let number of numbers){
+  resultado += number;
+  console.log(resultado);
+}
+
 // 3- Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
 // A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
 // 4- Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
