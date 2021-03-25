a457fb4af6948517b02ee8b9f4898330c948f9e9
Thyago Pessoa <flash_thyago@hotmail.com>
2021-03-25 11:01:57 -0300

    exercicio 7

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
@@ -88,13 +88,40 @@
 // Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
 // Exemplo: bishop -> diagonals 
 
-let chessPiece = "HoRsE";
-let chessPieceOrg = chessPiece.toLowerCase();
-let movement = "L";
-let msgMissingPiece = "Your piece could not be found.";
+// let chessPiece = "HoRsE";
+// let chessPieceOrg = chessPiece.toLowerCase();
+// let movement = "L";
+// let msgMissingPiece = "Your piece could not be found.";
 
-if (chessPieceOrg == "horse"){
-  console.log(movement);
-} else {
-  console.log(msgMissingPiece);
+// if (chessPieceOrg == "horse"){
+//   console.log(movement);
+// } else {
+//   console.log(msgMissingPiece);
+// }
+
+// 7- Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+// Porcentagem >= 90 -> A
+// Porcentagem >= 80 -> B
+// Porcentagem >= 70 -> C
+// Porcentagem >= 60 -> D
+// Porcentagem >= 50 -> E
+// Porcentagem < 50 -> F
+// O programa deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100. 
+
+let nota = 72;
+
+if(nota >= 90 && nota <= 100){
+  console.log("A");
+} else if (nota < 90 && nota >= 80){
+  console.log("B");
+} else if (nota < 80 && nota >= 70){
+  console.log("C");
+} else if (nota < 70 && nota >= 60){
+  console.log("D");
+} else if (nota < 60 && nota >= 50){
+  console.log("E");
+} else if (nota < 50){
+  console.log("F");
+} else if (nota > 100 || nota < 50){
+  console.log("Nota fora dos padrões");
 }
