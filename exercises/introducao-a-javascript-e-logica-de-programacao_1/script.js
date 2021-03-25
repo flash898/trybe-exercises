711b336a896b55b599165591f2ebf8c751488425
Thyago Pessoa <flash_thyago@hotmail.com>
2021-03-24 21:27:45 -0300

    exercicio 6

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
@@ -70,15 +70,31 @@
 
 //5- Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.//
 
-let angulo_1 = 30;
-let angulo_2 = 30;
-let angulo_3 = 120;
+// let angulo_1 = 30;
+// let angulo_2 = 30;
+// let angulo_3 = 120;
 
-let triangulo = angulo_1 + angulo_2 + angulo_3;
+// let triangulo = angulo_1 + angulo_2 + angulo_3;
 
-if (triangulo >= 180 && angulo_1 > 0 && angulo_2 > 0 && angulo_3 > 0 ){
-  console.log(true);
+// if (triangulo >= 180 && angulo_1 > 0 && angulo_2 > 0 && angulo_3 > 0 ){
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 6- Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+// Como desafio, faça o programa funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais.
+// Como dica, você pode pesquisar uma função que faz uma string ficar com todas as letras minúsculas (lower case) .
+// Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
+// Exemplo: bishop -> diagonals 
+
+let chessPiece = "HoRsE";
+let chessPieceOrg = chessPiece.toLowerCase();
+let movement = "L";
+let msgMissingPiece = "Your piece could not be found.";
+
+if (chessPieceOrg == "horse"){
+  console.log(movement);
 } else {
-  console.log(false);
+  console.log(msgMissingPiece);
 }
-
