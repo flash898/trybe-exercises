b6abde81983d52e20bd10fcc0ad9d2c5d4663203
Thyago Pessoa <flash_thyago@hotmail.com>
2021-03-26 00:07:53 -0300

    exercicio 7

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/script.js
@@ -19,7 +19,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // let resultado = 0;
 
-// for(let i=0; i < numbers.length; i++){
+// for(let i=0; i < numbers.length; i += 1){
 //   resultado += numbers[i];
 // }
 // console.log(resultado/numbers.length);
@@ -29,7 +29,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // let resultado = 0;
 // let sum;
 
-// for(let i=0; i < numbers.length; i++){
+// for(let i=0; i < numbers.length; i += 1){
 //   resultado += numbers[i];
 //   sum = resultado/numbers.length;
     
@@ -45,7 +45,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // 5- Utilizando for , descubra qual o maior valor contido no array e imprima-o;
 
 // let maior = 0;
-// for (let i=0; i < numbers.length; i++){
+// for (let i=0; i < numbers.length; i += 1){
 
 //     if(numbers[i] > maior){
 //       maior = numbers[i];
@@ -55,23 +55,33 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // 6- Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
 
-let odds = [];
+// let odds = [];
 
-for (let i =0; i<numbers.length;i++){
+// for (let i =0; i<numbers.length;i += 1){
 
-  if(numbers[i] % 2 != 0){
-    odds.push(numbers[i]);
-  }
-} 
+//   if(numbers[i] % 2 != 0){
+//     odds.push(numbers[i]);
+//   }
+// } 
 
-if (odds.length != 0){
-  console.log(odds);
-  console.log("Existem " + odds.length + " números ímpares!")
-} else {
-  console.log("Nenhum valor ímpar encontrado!");
-}
+// if (odds.length != 0){
+//   console.log(odds);
+//   console.log("Existem ", odds.length, " números ímpares!")
+// } else {
+//   console.log("Nenhum valor ímpar encontrado!");
+// }
 
 
 // 7- Utilizando for , descubra qual o menor valor contido no array e imprima-o;
+
+let menor = 0;
+for (let i=0; i < numbers.length; i += 1){
+// 
+    if(numbers[i] < numbers[i+1]){
+      menor = numbers[i];
+    }
+  }
+console.log(menor);
+
 // 8- Utilizando for , crie uma array que vá de 1 até 25 e imprima o resultado;
 // 9- Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
