16623aa9429a3e9450c6bc339cbca202e915a4e4
Thyago Pessoa <flash_thyago@hotmail.com>
2021-03-26 00:17:27 -0300

    exercicio 8

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/script.js
@@ -74,14 +74,21 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // 7- Utilizando for , descubra qual o menor valor contido no array e imprima-o;
 
-let menor = 0;
-for (let i=0; i < numbers.length; i += 1){
-// 
-    if(numbers[i] < numbers[i+1]){
-      menor = numbers[i];
-    }
-  }
-console.log(menor);
+// let menor = 0;
+// for (let i=0; i < numbers.length; i += 1){
+// // 
+//     if(numbers[i] < numbers[i+1]){
+//       menor = numbers[i];
+//     }
+//   }
+// console.log(menor);
 
 // 8- Utilizando for , crie uma array que vá de 1 até 25 e imprima o resultado;
+
+let novoArr = []
+for (let i=1; i <= 25; i += 1){
+  novoArr.push(i);
+}
+console.log(novoArr);
+
 // 9- Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
