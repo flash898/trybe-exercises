818c3a609957cf22778dbec005624353ae7b6ec6
Thyago Pessoa <flash_thyago@hotmail.com>
2021-03-29 21:53:14 -0300

    Exercício 5

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/script.js
@@ -22,6 +22,19 @@ info.recorrente = 'Sim';
 
 //4- Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto.
 
-for (let key in info){
-  console.log(info[key]);
-}
+// for (let key in info){
+//   console.log(info[key]);
+// }
+
+//5- Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
+
+let infoSecond = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+};
+
+for(let key in info){
+  console.log(info[key] + ' e ' +infoSecond[key]);
+};
