69c1b79835b676c9f92306d42ca2601a5d029404
Thyago Pessoa <flash_thyago@hotmail.com>
2021-03-29 21:42:39 -0300

    Exercício 2

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/script.js
@@ -1,9 +1,15 @@
-//1- Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
-// Valor esperado no console: Bem-vinda, Margarida
-
 let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
-console.log('Bem-vinda, ' + info.personagem);
+
+//1- Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
+// Valor esperado no console: Bem-vinda, Margarida
+
+// console.log('Bem-vinda, ' + info.personagem);
+
+//2- Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
+
+info.recorrente = 'Sim';
+console.log(info);
