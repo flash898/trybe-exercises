3658d876afdafd64602874d5d0b857b37e31a9c9
Thyago Pessoa <flash_thyago@hotmail.com>
2021-04-09 03:03:38 -0300

    Exercicio 8

--- exercises/5/5_3/script.js
+++ exercises/5/5_3/script.js
@@ -134,3 +134,16 @@ function addTasks(string){
   exerciseSeven.appendChild(exerciseSevenItem);
 }
 addTasks('Play basketball');
+
+/* 8- Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task .
+O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks".*/
+
+function legendColor(string){
+  let exerciseEigth = document.querySelector('.my-tasks');
+  let exerciseEigthItem = document.createElement('div');
+  exerciseEigthItem.className = 'task';
+  exerciseEigthItem.style.backgroundColor = string;
+  exerciseEigth.appendChild(exerciseEigthItem);
+}
+legendColor('red');
