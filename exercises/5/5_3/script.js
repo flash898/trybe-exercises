4e3915e7bc530cca8f0838114dd985c1137d9589
Thyago Pessoa <flash_thyago@hotmail.com>
2021-04-09 01:52:11 -0300

    Exercicio 4

--- exercises/5/5_3/script.js
+++ exercises/5/5_3/script.js
@@ -76,3 +76,16 @@ function changeButtonColor(){
   })
 }
 changeButtonColor();
+
+/* 4- Implemente uma função que receba como parâmetro a string "Sexta-feira" e crie dinamicamente um botão com o nome "Sexta-feira".
+Adicione a este botão o ID "btn-friday".
+Adicione este botão como filho/filha da tag <div> com classe "buttons-container". */
+
+function fridayButton(string){
+  let exerciseFour = document.querySelector('.buttons-container');
+  let exerciseFourItem = document.createElement('button');
+  exerciseFourItem.id = 'btn-friday';
+  exerciseFourItem.innerText = string;
+  exerciseFour.appendChild(exerciseFourItem);
+}
+fridayButton('Sexta-feira');
