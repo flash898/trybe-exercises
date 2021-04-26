3d2b8d125ab347b3c17165f89f03517a02c73474
Thyago Pessoa <flash_thyago@hotmail.com>
2021-04-26 19:13:19 -0300

    exercicio 3

--- exercises/7/7_1/introducao-a-javascript-es6-e-testes-unitarios.1/script.js
+++ exercises/7/7_1/introducao-a-javascript-es6-e-testes-unitarios.1/script.js
@@ -1,3 +1,4 @@
+/* exercicio 1 */
 const testingScope = escopo => {
   if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -11,8 +12,29 @@ const testingScope = escopo => {
 
 testingScope(false);
 
+/* exercicio 2 */
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 oddsAndEvens.sort((a, b) => a - b);
 
 console.log(oddsAndEvens);
+
+/* exercicio 3 */
+let fact = (number) => {
+  let sum = 1;
+  for (let i = 1; i <= number; i += 1) {
+    sum *= i;
+  }
+  return sum;
+}
+console.log(fact(4))
+
+let fact = (number) => {
+  let sum = 1;
+  for (let i = number; i > 0; i -= 1) {
+    sum *= i;
+  }
+
+  return sum;
+}
+console.log(fact(4))
