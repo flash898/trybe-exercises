149ff66a59a97e376584bb3cca05ace01bacba59
Thyago Pessoa <flash_thyago@hotmail.com>
2021-04-28 13:15:58 -0300

    exercicio 2.2

--- exercises/7/7_2/introducao-a-javascript-es6-e-testes-unitarios_2/lessons.js
+++ exercises/7/7_2/introducao-a-javascript-es6-e-testes-unitarios_2/lessons.js
@@ -20,4 +20,12 @@ const lesson3 = {
 
 /* Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela. */
 Object.assign(lesson2, {'turno': 'manhã'});
-console.log(lesson2);
+// console.log(lesson2);
+
+/* Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro. */
+const list = Object.keys(lesson2);
+let str = '';
+for (index in list) {
+  str += `${list[index]}\n`;
+}
+console.log(str);
