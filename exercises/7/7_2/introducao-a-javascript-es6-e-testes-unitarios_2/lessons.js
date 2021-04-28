aaec3ecdd2f20fbf1ca65c77f9ce99e62f1ba8b6
Thyago Pessoa <flash_thyago@hotmail.com>
2021-04-28 16:25:41 -0300

    exercicio 2.3.1

--- exercises/7/7_2/introducao-a-javascript-es6-e-testes-unitarios_2/lessons.js
+++ exercises/7/7_2/introducao-a-javascript-es6-e-testes-unitarios_2/lessons.js
@@ -18,18 +18,14 @@ const lesson3 = {
   turno: 'noite',
 };
 
-/* Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela. */
-Object.assign(lesson2, {'turno': 'manhã'});
-// console.log(lesson2);
+// /* Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela. */
+// Object.assign(lesson2, {'turno': 'manhã'});
+// // console.log(lesson2);
 
-/* Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro. */
+// /* Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro. */
 const list = (obj) => Object.keys(obj);
-// let str = '';
-// for (index in list) {
-//   str += `${list[index]}\n`;
-// }
 console.log(list(lesson2));
 
-/* Crie uma função para mostrar o tamanho de um objeto. */
+// /* Crie uma função para mostrar o tamanho de um objeto. */
 const listSize = (obj) => Object.keys(obj).length;
 console.log(listSize(lesson2));
