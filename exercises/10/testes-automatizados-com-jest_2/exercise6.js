59a836c162d843a9399c407625a0cc852aea1fa3
Thyago Pessoa <flash_thyago@hotmail.com>
2021-05-20 13:21:16 -0300

    exercicio 6.1

---
+++ exercises/10/testes-automatizados-com-jest_2/exercise6.js
@@ -0,0 +1,23 @@
+const Animals = [
+  { name: 'Dorminhoco', age: 1, type: 'Dog' },
+  { name: 'Soneca', age: 2, type: 'Dog' },
+  { name: 'Preguiça', age: 5, type: 'Cat' },
+];
+
+const findAnimalByName = (name) => {
+ return new Promise ((resolve, reject) => {
+    const animal = Animals.find((animal) => animal.name === name);
+    if(animal) return resolve(animal);
+    return reject('Nenhum animal com esse nome!');   
+  });
+};
+
+const getAnimal = (name) => {
+  return findAnimalByName(name).then(r => r)
+};
+
+module.exports = {
+  Animals,
+  findAnimalByName,
+  getAnimal
+}
