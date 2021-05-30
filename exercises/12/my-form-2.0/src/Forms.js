73ff132330f83bc677959e3b501659cc8a38766e
Thyago Pessoa <flash_thyago@hotmail.com>
2021-05-30 16:57:29 -0300

    Começando

---
+++ exercises/12/my-form-2.0/src/Forms.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Forms.css';
+
+class Forms extends React.Component {
+  constructor() {
+    super() 
+  }
+  render() {
+    return(
+      <div>
+        <h1>Meu Formulário 2.0</h1>
+        
+      </div>);
+  }
+}
+
+export default Forms;
