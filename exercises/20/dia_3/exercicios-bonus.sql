139420372214f4cc664d302c97045330a4149892
Thyago Pessoa <flash_thyago@hotmail.com>
2021-08-20 08:39:36 -0300

    exercicio bonus 3

--- exercises/20/dia_3/exercicios-bonus.sql
+++ exercises/20/dia_3/exercicios-bonus.sql
@@ -7,5 +7,11 @@ WHERE Name LIKE '%e%';
 -- 2- Escreva uma query para exibir o nome de todos os projetos cujo o código inicie com a letra A . Ordene o resultado em ordem alfabética.
 
 USE Scientists;
-SELECT * FROM Projects
+SELECT Name FROM Projects
 WHERE Code LIKE 'A%' ORDER BY Name;
+
+-- 3- Escreva uma query para exibir o código e nome de todos os projetos que possuam em seu código o número 3 . Ordene o resultado em ordem alfabética.
+
+USE Scientists;
+SELECT Code, Name FROM Projects
+WHERE Code LIKE '%3%' ORDER BY Name;
