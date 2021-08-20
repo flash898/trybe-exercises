e2f75aefdf19360f3205f7b235da134eb94ed622
Thyago Pessoa <flash_thyago@hotmail.com>
2021-08-20 13:06:32 -0300

    exercicio bonus 3

--- exercises/20/dia_4/exercicios-bonus.sql
+++ exercises/20/dia_4/exercicios-bonus.sql
@@ -13,3 +13,12 @@ SET SQL_SAFE_UPDATES = 0;
 UPDATE Pixar.BoxOffice
 SET rating = 6.0
 WHERE international_sales < 300000000 AND domestic_sales > 200000000;
+
+-- Exercício 10 : Exclua da tabela Movies todos os filmes com menos de 100 minutos de duração.
+
+SET SQL_SAFE_UPDATES = 0;
+
+DELETE FROM Pixar.BoxOffice
+WHERE movie_id IN (3, 4, 10);
+DELETE FROM Pixar.Movies
+WHERE length_minutes > 100;
