de2cd32fc5625d900ac731642a3dee6ecb723161
Thyago Pessoa <flash_thyago@hotmail.com>
2021-08-20 08:13:05 -0300

    exercicio 2

--- exercises/20/dia_3/exercicios.sql
+++ exercises/20/dia_3/exercicios.sql
@@ -6,5 +6,5 @@ SELECT * FROM Pecas WHERE name LIKE 'GR%';
 -- 2- Escreva uma query para exibir todos os fornecimentos que contenham a peça com code 2 . Organize o resultado por ordem alfabética de fornecedor.
 
 USE PecasFornecedores;
-SELECT * FROM Fornecimentos WHERE code = 2 ORDER BY Fornecedor;
+SELECT * FROM Fornecimentos WHERE peca = 2 ORDER BY Fornecedor;
 
