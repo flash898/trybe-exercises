821046c524eadf14dc7811b90010ef4b41d4fc2f
Thyago Pessoa <flash_thyago@hotmail.com>
2021-08-20 12:06:19 -0300

    exercicio 2

--- exercises/20/dia_4/exercicios.sql
+++ exercises/20/dia_4/exercicios.sql
@@ -6,3 +6,8 @@ VALUES
 	('Procurando Nemo', 'John Lasseter', 2003, 107),
 	('Os Incríveis', 'Brad Bird', 2004, 116),
 	('WALL-E', 'Pete Docter', 2008, 104);
+
+-- Exercício 2 : Procurando Nemo foi aclamado pela crítica! Foi classificado em 6.8, fez 450 milhões no mercado interno e 370 milhões no mercado internacional. Adicione as informações à tabela BoxOffice .
+
+INSERT INTO Pixar.BoxOffice (movie_id, rating, domestic_sales, international_sales)
+	VALUES(9, 6.8, 450000000, 370000000);
