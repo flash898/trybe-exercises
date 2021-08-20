d8c04787fd563d3dfaf8981882677aea06c59786
Thyago Pessoa <flash_thyago@hotmail.com>
2021-08-20 12:16:12 -0300

    exercicio 4

--- exercises/20/dia_4/exercicios.sql
+++ exercises/20/dia_4/exercicios.sql
@@ -18,3 +18,10 @@ SET SQL_SAFE_UPDATES = 0;
 UPDATE Pixar.Movies
 SET director = 'Andrew Staton'
 WHERE director = 'John Lasseter';
+
+-- Exercício 4 : O título do filme "Ratatouille" esta escrito de forma incorreta na tabela Movies , além disso, o filme foi lançado em 2007 e não em 2010. Corrija esses dados utilizando o UPDATE .
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE Pixar.Movies
+SET title = 'Ratatouille', year = 2007
+WHERE id = 3;
