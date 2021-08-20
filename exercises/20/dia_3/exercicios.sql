40cb9b03957d67dffa235906d5c43cc94e1f8fe1
Thyago Pessoa <flash_thyago@hotmail.com>
2021-08-20 08:25:47 -0300

    exercicio 7

--- exercises/20/dia_3/exercicios.sql
+++ exercises/20/dia_3/exercicios.sql
@@ -31,3 +31,9 @@ WHERE code LIKE '%F%';
 USE PecasFornecedores;
 SELECT * FROM Fornecimentos
 WHERE Preco BETWEEN 15 and 40 ORDER BY Preco;
+
+-- 7- Escreva uma query para exibir o número de vendas feitas entre o dia 15/04/2018 e o dia 30/07/2019 .
+
+USE PecasFornecedores;
+SELECT COUNT(*) FROM Vendas
+WHERE order_date BETWEEN '2018-04-15' and '2019-07-30';
