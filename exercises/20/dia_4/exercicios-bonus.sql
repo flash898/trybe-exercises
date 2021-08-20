8030611be31ff9cf83315b88c7240d8d5036a55a
Thyago Pessoa <flash_thyago@hotmail.com>
2021-08-20 13:01:54 -0300

    exercicio bonus 2

--- exercises/20/dia_4/exercicios-bonus.sql
+++ exercises/20/dia_4/exercicios-bonus.sql
@@ -5,3 +5,11 @@ SET SQL_SAFE_UPDATES = 0;
 UPDATE Pixar.BoxOffice
 SET rating = 9.0
 WHERE domestic_sales > 400000000;
+
+-- Exercício 9 : Altere a classificação da tabela BoxOffice para 6.0 de todos os filmes que lucraram menos de 300 milhões no mercado internacional e mais de 200 milhões no mercado interno.
+
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE Pixar.BoxOffice
+SET rating = 6.0
+WHERE international_sales < 300000000 AND domestic_sales > 200000000;
