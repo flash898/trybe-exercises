a8c88cb6684093e7e391db4cad27cede715ebcc8
Thyago Pessoa <flash_thyago@hotmail.com>
2021-08-25 13:01:01 -0300

    Exercicio 8

--- exercises/21/dia_1/exercicios.sql
+++ exercises/21/dia_1/exercicios.sql
@@ -24,3 +24,7 @@ SELECT JOB_ID, COUNT(*) AS PROG FROM hr.employees WHERE JOB_ID = 'IT_PROG';
 -- 7. Escreva uma query que exiba a quantidade de dinheiro necessária para efetuar o pagamento de cada profissão ( JOB_ID ).
 
 SELECT JOB_ID, SUM(SALARY) AS TOTAL_PAYMENT FROM hr.employees GROUP BY JOB_ID;
+
+-- 8. Utilizando a query anterior, faça as alterações para que seja exibido somente a quantidade de dinheiro necessária para cobrir a folha de pagamento das pessoas programadoras ( IT_PROG ).
+
+SELECT JOB_ID, SUM(SALARY) AS TOTAL_PAYMENT FROM hr.employees WHERE JOB_ID = 'IT_PROG';
