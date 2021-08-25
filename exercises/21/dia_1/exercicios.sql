e62340b4a9083b8af08711d0e73a6fa503bb6463
Thyago Pessoa <flash_thyago@hotmail.com>
2021-08-25 13:32:18 -0300

    Exercicio 14

--- exercises/21/dia_1/exercicios.sql
+++ exercises/21/dia_1/exercicios.sql
@@ -50,3 +50,7 @@ SELECT * FROM hr.employees WHERE LENGTH(FIRST_NAME) >= 8 ;
 -- 13. Escreva uma query que exiba as seguintes informações de cada funcionário: id , primeiro nome e ano no qual foi contratado (exiba somente o ano).
 
 SELECT EMPLOYEE_ID, FIRST_NAME, YEAR(HIRE_DATE) FROM hr.employees;
+
+-- 14. Escreva uma query que exiba as seguintes informações de cada funcionário: id , primeiro nome e dia do mês no qual foi contratado (exiba somente o dia).
+
+SELECT EMPLOYEE_ID, FIRST_NAME, MONTH(HIRE_DATE) FROM hr.employees;
