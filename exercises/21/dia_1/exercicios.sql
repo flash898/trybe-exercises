e061ce070283de3eac8d5e1fe56462d7bff2ae34
Thyago Pessoa <flash_thyago@hotmail.com>
2021-08-25 13:31:18 -0300

    Exercicio 13

--- exercises/21/dia_1/exercicios.sql
+++ exercises/21/dia_1/exercicios.sql
@@ -46,3 +46,7 @@ SELECT * FROM hr.employees;
 -- 12. Escreva uma query que só exiba as informações dos funcionários cujo o primeiro nome tenha oito ou mais caracteres.
 
 SELECT * FROM hr.employees WHERE LENGTH(FIRST_NAME) >= 8 ;
+
+-- 13. Escreva uma query que exiba as seguintes informações de cada funcionário: id , primeiro nome e ano no qual foi contratado (exiba somente o ano).
+
+SELECT EMPLOYEE_ID, FIRST_NAME, YEAR(HIRE_DATE) FROM hr.employees;
