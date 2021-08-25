1089ab17fae8247041c728ed66c10b0af93da040
Thyago Pessoa <flash_thyago@hotmail.com>
2021-08-25 12:37:24 -0300

    Exercicio 2

--- exercises/21/dia_1/exercicios.sql
+++ exercises/21/dia_1/exercicios.sql
@@ -1,2 +1,6 @@
 -- 1. Escreva uma query que exiba o maior salário da tabela.
 SELECT FIRST_NAME, MAX(SALARY) AS BIGGEST_SALARY FROM hr.employees;
+
+-- 2. Escreva uma query que exiba a diferença entre o maior e o menor salário.
+
+SELECT MAX(SALARY) - MIN(SALARY) AS DIFF_SALARY FROM hr.employees;
