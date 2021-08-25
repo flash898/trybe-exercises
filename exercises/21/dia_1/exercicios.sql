52d608fad6006d2f4191102fa4fdacc61c887caf
Thyago Pessoa <flash_thyago@hotmail.com>
2021-08-25 12:44:47 -0300

    Exercicio 3

--- exercises/21/dia_1/exercicios.sql
+++ exercises/21/dia_1/exercicios.sql
@@ -4,3 +4,7 @@ SELECT FIRST_NAME, MAX(SALARY) AS BIGGEST_SALARY FROM hr.employees;
 -- 2. Escreva uma query que exiba a diferença entre o maior e o menor salário.
 
 SELECT MAX(SALARY) - MIN(SALARY) AS DIFF_SALARY FROM hr.employees;
+
+-- 3. Escreva uma query que exiba a média salarial de cada JOB_ID , ordenando pela média salarial em ordem decrescente.
+
+SELECT JOB_ID, AVG(SALARY) AS AVERAGE_SALARY FROM hr.employees GROUP BY JOB_ID ORDER BY AVERAGE_SALARY DESC;
