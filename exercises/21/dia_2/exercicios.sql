fdc70d96aac9397cba0f80f9f72d53ae07debb9f
Thyago Pessoa <flash_thyago@hotmail.com>
2021-08-26 08:46:52 -0300

    Exercicio 4

--- exercises/21/dia_2/exercicios.sql
+++ exercises/21/dia_2/exercicios.sql
@@ -9,3 +9,7 @@ SELECT m.title, b.domestic_sales, b.international_sales FROM Pixar.BoxOffice AS 
 -- Exercício 3: Utilizando o INNER JOIN , faça uma busca que retorne os filmes e sua avaliação ( rating ) em ordem decrescente.
 
 SELECT m.title, b.rating FROM Pixar.BoxOffice AS b INNER JOIN Movies AS m ON m.id = b.movie_id ORDER BY m.title DESC;
+
+-- Exercício 4: Utilizando o LEFT JOIN , faça uma busca que retorne todos os dados dos cinemas, mesmo os que não possuem filmes em cartaz e, adicionalmente, os dados dos filmes que estão em cartaz nestes cinemas. Retorne os nomes dos cinemas em ordem alfabética.
+
+SELECT m.title, m.director, m.`year`, m.length_minutes, t.`name`, t.location FROM Pixar.Theater AS t LEFT JOIN Movies AS m ON t.id = m.theater_id ORDER BY t.`name` ASC;
