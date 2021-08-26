71aa7ea3cf43502e70653bea4e985ec45e9eac5f
Thyago Pessoa <flash_thyago@hotmail.com>
2021-08-26 08:54:28 -0300

    Exercicio Bonus 2

--- exercises/21/dia_2/exercicios-bonus.sql
+++ exercises/21/dia_2/exercicios-bonus.sql
@@ -1,3 +1,7 @@
 -- Exercício 10: Utilizando o INNER JOIN , selecione todas as informações dos filmes com avaliação maior que 8 e que estejam em cartaz.
 
 SELECT m.id, m.title, m.director, m.`year`, m.length_minutes, m.theater_id  FROM Pixar.Movies AS m INNER JOIN BoxOffice AS b ON m.id = b.movie_id WHERE b.rating > 8 AND m.theater_id IS NOT NULL;
+
+-- Exercício 11: Utilizando o SELF JOIN , selecione os títulos e duração dos filmes que possuem o mesmo diretor.
+
+SELECT m1.title, m1.length_minutes, m2.title, m2.length_minutes FROM Pixar.Movies AS m1, Pixar.Movies AS m2 WHERE m1.director = m2.director;
