bf7512e1856e242c37184503389d12ec8f730fd2
Thyago Pessoa <flash_thyago@hotmail.com>
2021-08-26 08:46:01 -0300

    Exercicio 3

--- exercises/21/dia_2/exercicios.sql
+++ exercises/21/dia_2/exercicios.sql
@@ -5,3 +5,7 @@ SELECT m.title, b.domestic_sales, b.international_sales FROM Pixar.BoxOffice AS 
 -- Exercício 2: Utilizando o INNER JOIN , faça uma busca que retorne o número de vendas para cada filme que possui um número maior de vendas internacionais ( international_sales ) do que vendas nacionais ( domestic_sales ).
 
 SELECT m.title, b.domestic_sales, b.international_sales FROM Pixar.BoxOffice AS b INNER JOIN Movies AS m ON m.id = b.movie_id WHERE b.international_sales > b.domestic_sales;
+
+-- Exercício 3: Utilizando o INNER JOIN , faça uma busca que retorne os filmes e sua avaliação ( rating ) em ordem decrescente.
+
+SELECT m.title, b.rating FROM Pixar.BoxOffice AS b INNER JOIN Movies AS m ON m.id = b.movie_id ORDER BY m.title DESC;
