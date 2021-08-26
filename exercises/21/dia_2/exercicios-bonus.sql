1bee179de8bb249ea735751bde50d91c97c950e0
Thyago Pessoa <flash_thyago@hotmail.com>
2021-08-26 08:54:55 -0300

    Exercicio Bonus 3

--- exercises/21/dia_2/exercicios-bonus.sql
+++ exercises/21/dia_2/exercicios-bonus.sql
@@ -5,3 +5,7 @@ SELECT m.id, m.title, m.director, m.`year`, m.length_minutes, m.theater_id  FROM
 -- Exercício 11: Utilizando o SELF JOIN , selecione os títulos e duração dos filmes que possuem o mesmo diretor.
 
 SELECT m1.title, m1.length_minutes, m2.title, m2.length_minutes FROM Pixar.Movies AS m1, Pixar.Movies AS m2 WHERE m1.director = m2.director;
+
+-- Exercício 12: Faça duas buscas, uma utilizando SUBQUERY e outra utilizando INNER JOIN , que retornem o título dos filmes que arrecadaram 500 milhões ou mais, e que possuem duração maior que 110 minutos.
+
+SELECT m.title FROM Pixar.Movies AS m WHERE m.id IN (SELECT b.movie_id FROM Pixar.BoxOffice AS b WHERE b.international_sales >= 500000000 AND m.length_minutes > 110);
