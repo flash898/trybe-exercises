ae04193d8b8b343df728c479ecd2a9dd07ac4801
Thyago Pessoa <flash_thyago@hotmail.com>
2021-09-26 12:05:34 -0300

    exercicio 2

--- exercises/26/dia_2/index.js
+++ exercises/26/dia_2/index.js
@@ -1,27 +1,33 @@
-const readline = require('readline-sync');
+const num1 = Math.floor(Math.random() * 100 + 1);
+const num2 = Math.floor(Math.random() * 100 + 1);
+const num3 = Math.floor(Math.random() * 10 + 1);
 
-const num1 = readline.questionInt('Digite o primeiro número');
-const num2 = readline.questionInt('Digite o segundo número');
-const num3 = readline.questionInt('Digite o terceiro número');
+let resultado;
+ 
+console.log(num1, num2, num3);
 
-function calcularNumeros() {
+function calcularNumeros(num1, num2, num3) {
 
   const promise = new Promise((resolve, reject) => {
     if( typeof num1 != 'number' || typeof num2 != 'number' || typeof num3 != 'number') {
       reject(new Error('Informe apenas números'))
     }
 
-    const resultado = (num1 + num2) / num3;
+    resultado = (num1 + num2) / num3;
 
     if(resultado < 50) {
       reject(new Error('Valor muito baixo'))
     }
-    
+
     resolve(resultado);
-    console.log(resultado);
   })
 
   return promise;
 }
 
-calcularNumeros();
+try {
+  calcularNumeros(num1, num2, num3);
+  console.log(resultado);
+} catch (e) {
+  console.log(e.message);
+}
