61651e5094fe8167b7f61231287dd063b0809925
Thyago Pessoa <flash_thyago@hotmail.com>
2021-09-26 12:12:35 -0300

    exercicio 2- correçao

--- exercises/26/dia_2/index.js
+++ exercises/26/dia_2/index.js
@@ -3,7 +3,7 @@ const num2 = Math.floor(Math.random() * 100 + 1);
 const num3 = Math.floor(Math.random() * 10 + 1);
 
 let resultado;
- 
+
 console.log(num1, num2, num3);
 
 function calcularNumeros(num1, num2, num3) {
@@ -25,9 +25,6 @@ function calcularNumeros(num1, num2, num3) {
   return promise;
 }
 
-try {
-  calcularNumeros(num1, num2, num3);
-  console.log(resultado);
-} catch (e) {
-  console.log(e.message);
-}
+calcularNumeros(num1, num2, num3)
+    .then(result => console.log(result))
+    .catch (e => console.log(e.message));
