21dbb654a510ab32fd9959175744c197fbe538a5
Thyago Pessoa <flash_thyago@hotmail.com>
2021-09-30 11:46:26 -0300

    exercicio 5 e 6

--- exercises/26/dia_4/index.js
+++ exercises/26/dia_4/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+const e = require('express');
 
 const app = express();
 app.use(bodyParser.json());
+
+async function readSimpsons(){
+  try {
+    const simpsons = await fs.readFile('simpsons.json', 'utf-8');
+    console.log(simpsons);
+    return JSON.parse(simpsons);
+  } catch (error) {
+    return error.message;
+  }
+}
+
+function writeSimpsons(newChar) {
+  const newSimpson = fs.writeFile('simpsons.json', JSON.stringify(newChar));
+  return newSimpson;
+}
 
 app.get('/ping', function(_req, res) {
   res.status(200).json({ message: 'pong' });
@@ -22,6 +39,13 @@ app.post('/greetings', function(req, res){
 app.put('/users/:name/:age', function(req, res){
   const { name, age } = req.body;
   res.status(200).json({ message: `Seu nome é ${name} e você tem ${age} anos de idade`});
+});
+
+
+app.get('/simpsons', async function(_req, res){
+  const simpsonsArr = await readSimpsons();
+
+  res.status(200).json(simpsonsArr);
 });
 
 app.listen(3002, () => {
