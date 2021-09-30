6c29e04bf20ef56c4747271c2bc4c53d54a13e19
Thyago Pessoa <flash_thyago@hotmail.com>
2021-09-30 10:30:50 -0300

    exercicio 2

--- exercises/26/dia_4/index.js
+++ exercises/26/dia_4/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/ping', function(_req, res) {
   res.status(200).json({ message: 'pong' });
+});
+
+app.post('/hello', function(req, res) {
+  const { username } = req.body; 
+  res.status(201).json({ message: `Hello, ${username}!`})
 });
 
 app.listen(3002, () => {
