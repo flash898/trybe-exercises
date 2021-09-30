49b02aac6f1f16e1d6cca21f2a2596171ea5bf1b
Thyago Pessoa <flash_thyago@hotmail.com>
2021-09-30 11:16:08 -0300

    exercicio 4

--- exercises/26/dia_4/index.js
+++ exercises/26/dia_4/index.js
@@ -19,6 +19,11 @@ app.post('/greetings', function(req, res){
   res.status(401).json({ message: "Unauthorized"});
 });
 
+app.put('/users/:name/:age', function(req, res){
+  const { name, age } = req.body;
+  res.status(200).json({ message: `Seu nome é ${name} e você tem ${age} anos de idade`});
+});
+
 app.listen(3002, () => {
   console.log('Aplicação na porta 3002');
 });
